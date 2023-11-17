9e588837f19d3836487306f7ce35c219aa3826f7
vic02505 <vzacarias@fi.uba.ar>
2023-11-17 12:36:52 -0300

    creo nueva rama

---
+++ build/CMakeFiles/libserver.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/Client.cpp" "CMakeFiles/libserver.dir/server/src/Client.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/Client.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/ClientSender.cpp" "CMakeFiles/libserver.dir/server/src/ClientSender.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/ClientSender.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/Game.cpp" "CMakeFiles/libserver.dir/server/src/Game.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/Game.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/MatchesMonitor.cpp" "CMakeFiles/libserver.dir/server/src/MatchesMonitor.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/MatchesMonitor.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/Server.cpp" "CMakeFiles/libserver.dir/server/src/Server.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/Server.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/TurnHandler.cpp" "CMakeFiles/libserver.dir/server/src/TurnHandler.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/TurnHandler.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/TurnMonitor.cpp" "CMakeFiles/libserver.dir/server/src/TurnMonitor.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/TurnMonitor.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/GameWorld.cpp" "CMakeFiles/libserver.dir/server/src/world/GameWorld.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/GameWorld.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/entities/WWorm.cpp" "CMakeFiles/libserver.dir/server/src/world/entities/WWorm.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/entities/WWorm.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/InstructionFactory.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/InstructionFactory.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/InstructionFactory.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormAttack.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormAttack.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormAttack.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormJump.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormJump.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormJump.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormMove.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormMove.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormMove.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormNone.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormNone.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormNone.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormSetWeapon.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormSetWeapon.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormSetWeapon.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormStopMove.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormStopMove.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormStopMove.cpp.o.d"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/world/instructions/impl/WormUnSetWeapon.cpp" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormUnSetWeapon.cpp.o" "gcc" "CMakeFiles/libserver.dir/server/src/world/instructions/impl/WormUnSetWeapon.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/build/CMakeFiles/libcommon.dir/DependInfo.cmake"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/build/SDL2pp/CMakeFiles/SDL2pp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/worms-server.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/server/src/main.cpp" "CMakeFiles/worms-server.dir/server/src/main.cpp.o" "gcc" "CMakeFiles/worms-server.dir/server/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/build/CMakeFiles/libserver.dir/DependInfo.cmake"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/build/CMakeFiles/libcommon.dir/DependInfo.cmake"
+  "/home/vic/Escritorio/universidad/materias/taller_i/tps/worms/build/SDL2pp/CMakeFiles/SDL2pp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
